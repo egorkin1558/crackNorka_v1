348572925308d389d6b34f54c448ed6ec459e9b9
egorkin1558 <94108695+egorkin1558@users.noreply.github.com>
2023-01-18 08:17:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def crack():
 
 @app.errorhandler(404)
 def not_found(error):
-    return '{"ok":true,"code":200,"message":"Sussec","forever":false,"timestamp":1676610130,"datetime":"2023-02-17T21:50:30.000000Z"}'
+    return '{"ok":true,"code":200,"message":"Sussec","forever":false,"timestamp":1676610130,"datetime":"2023-02-18T21:50:30.000000Z"}'
 
 context = ('serf.crt', 'key.key')#cs
 app.run(debug=True,host="0.0.0.0",port=443,ssl_context=context)
